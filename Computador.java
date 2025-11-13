9acc8a1451f538e1c4b159ff8cc4f2a9be60acdc
Gabriel Marafante <BIBIMARAFANTE@GMAIL.COM>
2025-11-12 23:04:14 -0300

    Create Computador class with basic functionality

---
+++ Computador.java
@@ -0,0 +1,70 @@
+package Entidades;
+
+public class Computador {
+
+	private String numeroSerie;
+    private String modelo;
+    private int memoriaRAM;
+    private boolean statusLigado;
+    
+    
+    public Computador(String numeroSerie, String modelo, int memoriaRam ){
+		this.modelo = modelo;
+		this.numeroSerie = numeroSerie;
+		this.memoriaRAM = memoriaRam;
+		this.statusLigado = false;
+	}
+    
+    public void ligar() {
+    	if (this.statusLigado == false) {
+            this.statusLigado = true;
+            System.out.println("O computador " + this.modelo + " foi ligado");
+
+        } else {
+            System.out.println("O computador " + this.modelo + " ja esta ligado");
+        }
+    }
+    
+    
+    public void desligar() {
+    	if (this.statusLigado = true) {
+    		this.statusLigado = false;
+    		System.out.println(" O computador " + this.modelo + " foi desligado ");
+    	}
+    	else {
+    		System.out.println(" O comptaudor " + this.modelo + " ja estava desligado ");
+    	}
+    }
+    
+    public boolean isligar() {
+    	return this.statusLigado;
+    }
+    
+    public boolean isDesligar() {
+    	return this.statusLigado;
+    }
+    
+    public String toString() {
+    	return 
+    	" Modelo: " +  this.modelo + 
+    	" Numero de serie: " +  this.numeroSerie + 	
+    	" Memoria ram " + this.memoriaRAM + 
+    	" Status: " + (this.statusLigado ? "LIGADOR " :"DESLIGADO");
+    	
+    }
+    
+    	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+	
+	
+
