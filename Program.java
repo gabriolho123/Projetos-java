aff327c610da4ead4503e094136c3c1ff3012d72
Gabriel Marafante <BIBIMARAFANTE@GMAIL.COM>
2025-11-12 23:04:38 -0300

    Add Program class to test Computador functionality

---
+++ Program.java
@@ -0,0 +1,64 @@
+package Electronic;
+
+import java.util.Scanner;
+
+import Entidades.Computador;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Computador pc = new Computador("SN-12345", "Notebook Pro", 16);
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("---- Teste de funcionalidade ----");
+
+	
+		System.out.println("Estado inicial (Desligado): " + pc.isligar());
+
+		
+		pc.ligar(); 
+
+		
+		System.out.println("Status após primeira ação: " + pc.isligar());
+
+		
+		pc.ligar(); 
+
+		
+		pc.desligar(); 
+
+	
+		System.out.println("Estado final: (ligado) " + pc.isligar());
+		
+		pc.desligar();
+		
+		System.out.println("-------- Config do pc --------" );
+		System.out.println( pc.toString());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
